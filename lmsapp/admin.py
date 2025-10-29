ded1fbc38abbd2d869732973bce32850cbf2aa59
haripriya845 <haripriyaviswanathan78@gmail.com>
2025-10-29 12:55:46 +0530

    final commit

--- lmsapp/admin.py
+++ lmsapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import student,teacher,leave
 # Register your models here.
+admin.site.register(student)
+admin.site.register(teacher)
+admin.site.register(leave)
+
+admin.site.site_header = "Leave Management System Admin"
